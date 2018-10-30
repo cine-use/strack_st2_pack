53e2475db1e52f344b9f8033228163736e346452
Aaron <guoliangxu1987@163.com>
2018-10-30 15:56:17 +0800

    Initial commit

---
+++ actions/sync_sg_to_strack.py
@@ -0,0 +1,11 @@
+from st2actions.runners.pythonrunner import Action 
+
+class WriteDataAction(Action):
+    def __init__(self,config):
+        pass
+        
+    def run(self,json_str):
+        # parse shotgun event info
+        
+        # do same option via strack api
+        pass
