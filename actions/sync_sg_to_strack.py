4b1037fd0c1f827010feb9c62dc55f10d1af481a
Aaron <guoliangxu1987@163.com>
2018-10-31 15:22:33 +0800

    debug

--- actions/sync_sg_to_strack.py
+++ actions/sync_sg_to_strack.py
@@ -12,15 +12,7 @@ class WriteDataAction(Action):
         super(WriteDataAction, self).__init__(config)
 
     def run(self, json_str):
-        st = self.action_service.get_value(name="strack_session")
-        if not st:
-            st = get_dam_session("strack")
-            self.action_service.set_value(name="strack_session", value=st)
-
-        sg = self.action_service.get_value(name="shotgun_session")
-        if not sg:
-            sg = get_dam_session("shotgun")
-            self.action_service.set_value(name="shotgun_session", value=sg)
+        st = get_dam_session("strack")
 
         self.logger.info("get event from shotgun: %s" % json_str)
         try:
@@ -36,29 +28,29 @@ class WriteDataAction(Action):
             # if anything created, cache entity type
             if event_meta_data.get("type") == "new_entity":  # start create
                 self.logger.debug("creating")
-                self.action_service.set_value(name="in_creating", value=True)
+                self.action_service.set_value(name="in_creating", value="true")
                 new_creation = {"module": event_meta_data.get("entity_type"),
                                 "fields": {"source_id": event_meta_data.get("entity_id")}}
-                self.action_service.set_value(name="new_creation", value=new_creation)
+                self.action_service.set_value(name="new_creation", value=json.dumps(new_creation))
                 return True, "Pending create, will deferred util creating done."
 
             # if something created, store attributes and create it util attr set done
             elif event_meta_data.get("type") == "attribute_change" and event_meta_data.get("in_create"):  # in creating
-                new_creation = self.action_service.get_value(name="new_creation")
+                new_creation = json.loads(self.action_service.get_value(name="new_creation"))
                 field_name = get_st_field(event_meta_data.get("entity_type"), event_meta_data.get("attribute_name"))
                 field_value = event_meta_data.get('new_value')
                 new_creation["fields"].setdefault(field_name, field_value)
-                self.action_service.set_value(name="new_creation", value=new_creation)
+                self.action_service.set_value(name="new_creation", value=json.dumps(new_creation))
                 return True, "Pending create, will deferred util creating done."
 
             # do create
-            elif self.action_service.get_value(name="in_creating"):  # just created
-                new_creation = self.action_service.get_value("new_creation")
+            elif self.action_service.get_value(name="in_creating") == "true":  # just created
+                new_creation = json.loads(self.action_service.get_value("new_creation"))
                 module_name = new_creation.get("module")
                 init_data = new_creation.get("fields")
                 st.create(module_name, init_data)       # FIXME: if entity in init data, it'll be wrong
                 self.logger.debug("create done")
-                self.action_service.set_value(name="in_creating", value=False)
+                self.action_service.set_value(name="in_creating", value="false")
                 return True, "new %s created, data: %s" % (module_name, init_data)
 
             # edit fields
